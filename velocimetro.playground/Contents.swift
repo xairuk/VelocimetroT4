752f450c343a5940b0777625ed79430e58906fe7
Cris  <cristobals@oemoda.com>
2015-12-28 22:40:13 -0600

    tarea

--- velocimetro.playground/Contents.swift
+++ velocimetro.playground/Contents.swift
@@ -1,15 +1,56 @@
 //: Tarea 4 Velocimetro
-//Prueba GitHub...diciembre
+//: test2 github
+
 
 import UIKit
 
-enum velocidades : Int {
-    case
-    Apagado = 0,
-    VelocidadBaja = 20,
-    VelodidadMedia = 50,
-    VelocidadAlta = 120
+enum Velocidades: Int {
+    
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init (velocidadInicial: Velocidades) {self = velocidadInicial
+    }
 }
 
-var velocidad = velocidades.Apagado
-velocidad.rawValue
+
+class Auto {
+    var velocidad: Velocidades
+    init() {
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
+    }
+    
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena: String) {
+        switch velocidad {
+        case .Apagado :
+            velocidad = .VelocidadBaja
+            return (velocidad.rawValue, "Velocidad Baja")
+            
+        case .VelocidadBaja:
+            velocidad = .VelocidadMedia
+            return (velocidad.rawValue, "Velocidad Media")
+            
+        case .VelocidadMedia:
+            velocidad = .VelocidadAlta
+            return (velocidad.rawValue, "Velocidad Alta")
+            
+        case .VelocidadAlta:
+            velocidad = .VelocidadMedia
+            return (velocidad.rawValue, "Velocidad Media")
+        }
+    }
+}
+
+
+var auto:Auto = Auto()
+
+print("\(auto.velocidad.rawValue), \(auto.velocidad)")
+
+var vel:(actual: Int, velocidadEnCadena: String)
+
+for index in 1...20 {
+    vel = auto.cambioDeVelocidad()
+    print("\(vel.actual), \(vel.velocidadEnCadena)")
+}
